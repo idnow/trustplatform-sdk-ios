33768d939621ef0b3032e0b5be3bdab8721c53fa
trustplatform-developer <trustplatform-developer@idnow.io>
2026-09-07 11:10:19 +0200

    Release - Update SDK to version 0.5.4

--- Package.swift
+++ Package.swift
@@ -23,13 +23,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TrustPlatform",
-            url: "https://github.com/idnow/trustplatform-sdk-ios/releases/download/0.5.4-beta/TrustPlatform.xcframework.zip",
-            checksum: "447071596d887fccd3a13a4f7d71fa0073c14f1a86b0b5fc6c00739d5bae2366"
+            url: "https://github.com/idnow/trustplatform-sdk-ios/releases/download/0.5.4/TrustPlatform.xcframework.zip",
+            checksum: "53475e7aa7ee3af48974e215565edc03436d6e77a303f29b15c4b04193b8dc14"
         ),
         .binaryTarget(
             name: "TrustPlatformDocIDV",
-            url: "https://github.com/idnow/trustplatform-sdk-ios/releases/download/0.5.4-beta/TrustPlatformDocIDV.xcframework.zip",
-            checksum: "18614b54748afaae5b655c22f4620658d3a6b02badd4179c96e6cc994e3a0fbf"
+            url: "https://github.com/idnow/trustplatform-sdk-ios/releases/download/0.5.4/TrustPlatformDocIDV.xcframework.zip",
+            checksum: "6a00cbb7bfa9fea8348f5a0e91ab4b2c45b6c695b5e791fd3e19f854226d9db2"
         ),
         .target(
             name: "TrustPlatform-Target-Wrapper",
